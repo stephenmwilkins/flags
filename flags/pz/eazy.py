da1bda446eb3f3033725c447c1f0b596d46f54da
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-08-24 14:09:22 +0100

    updated

--- flags/pz/eazy.py
+++ flags/pz/eazy.py
@@ -11,96 +11,139 @@ from astropy.io import ascii
 
 class Eazy():
 
-    def __init__(self, id, fc, EAZY_working_dir='EAZY', path_to_EAZY=os.getenv('EAZY'), create_POFZ_FILE=False):
+    def __init__(self, id, fc, params = None, eazy_working_dir='eazy', path_to_eazy=os.getenv('EAZY'), create_POFZ_FILE=False):
 
-        self.ID = id
+        self.id = id
         self.fc = fc
+
+        print(self.id)
 
         # --- create EAZY working directory if it doesn't already exist
 
-        self.EAZY_working_dir = EAZY_working_dir
-        self.path_to_EAZY = path_to_EAZY
+        self.eazy_working_dir = eazy_working_dir
+        self.path_to_eaxy = path_to_eazy
 
         self.create_POFZ_FILE = create_POFZ_FILE
 
-        if not os.path.exists(EAZY_working_dir):
-            os.mkdir(EAZY_working_dir)
-            os.mkdir(f'{EAZY_working_dir}/inputs')
-            os.mkdir(f'{EAZY_working_dir}/outputs')
+        if not os.path.exists(eazy_working_dir):
+            os.mkdir(eazy_working_dir)
+            os.mkdir(f'{eazy_working_dir}/inputs')
+            os.mkdir(f'{eazy_working_dir}/outputs')
+            print('created EAZY working directory')
 
             # --- create symbolic link to templates
         if not os.path.exists(f'templates'):
             try:
-                os.symlink(f'{path_to_EAZY}/templates', f'templates', target_is_directory=True)
+                os.symlink(f'{path_to_eazy}/templates', f'templates', target_is_directory=True)
+                print('Created link to EAZY templates')
             except:
-                print('tried to create a templates link')
+                print('Failed to create link to EAZY templates')
 
-        self.create_param()
+
+        # if no parameter dictionary supplied, use the default. NOT yet implemented
+        if not params:
+            print('NO PARAMETER DICTIONARY PROVIDED, DEFAULTS NOT YET IMPLEMENTED')
+        else:
+            self.params = params
+
+
+        # change some defaults
+        self.params['CATALOG_FILE'] = f'{self.eazy_working_dir}/inputs/{self.id}.cat'
+        self.params['MAIN_OUTPUT_FILE'] = f'{self.id}'
+        self.params['FILTERS_RES'] = f'{self.eazy_working_dir}/inputs/{self.id}.RES'
+
+        if self.create_POFZ_FILE:
+            self.params['POFZ_FILE'] = 'y'
+
+
+        for k, v in self.params.items():
+            print(f'{k} : {v}')
+    
+        # create filter resource file
         self.create_filter_RES()
 
     def create_filter_RES(self):
         """ Take a flare filter object and create a EAZY RES file """
 
-        self.filters = self.fc.filters
+        self.filters = self.fc.filter_codes
 
         # --- create filter RES file
         create_EAZY_filter_res(
-            self.fc, filter_res_file=f'{self.EAZY_working_dir}/inputs/{self.ID}.RES')
+            self.fc, filter_res_file=f'{self.eazy_working_dir}/inputs/{self.id}.RES')
 
     def run(self):
         """ Saves the input file and runs EAZY """
 
         # --- write the parameter file to the inputs directory
-        write_param_file(f'{self.EAZY_working_dir}/inputs/{self.ID}.param', self.params)
+        write_param_file(f'{self.eazy_working_dir}/inputs/{self.id}.param', self.params)
 
         # --- run EAZY
 
-        eazy_cmd = f'{self.path_to_EAZY}/src/eazy -p {self.EAZY_working_dir}/inputs/{self.ID}.param'
+        eazy_cmd = f'{self.path_to_eaxy}/src/eazy -p {self.eazy_working_dir}/inputs/{self.id}.param'
         print(eazy_cmd)
 
         os.system(eazy_cmd)
 
         # --- read EAZY output
-        # return get_EAZY_output_as_HDF5(f'{self.EAZY_working_dir}/outputs/{self.ID}', read_POFZ_FILE = self.create_POFZ_FILE, output_file = output_file)
+        # return get_EAZY_output_as_HDF5(f'{self.eazy_working_dir}/outputs/{self.id}', read_POFZ_FILE = self.create_POFZ_FILE, output_file = output_file)
 
-    def create_param(self):
-        """ Create a default parameter dictionary """
+   
+        
 
-        self.params = default_params(self.EAZY_working_dir)
-        self.params['CATALOG_FILE'] = f'{self.EAZY_working_dir}/inputs/{self.ID}.cat'
-        self.params['MAIN_OUTPUT_FILE'] = f'{self.ID}'
-        self.params['FILTERS_RES'] = f'{self.EAZY_working_dir}/inputs/{self.ID}.RES'
+    def create_input_catalogue_from_HDF5(self, hf, flux_path=lambda f: f'obs/{f}/flux', flux_err_path = lambda f: f'obs/{f}/flux_err'):
 
-        if self.create_POFZ_FILE:
-            self.params['POFZ_FILE'] = 'y'
+        """
+        Create an EAZY input from a HDF5 file by giving it a function that points to the flux and error as a function of the filter.
+        """
 
-    def create_input_catalogue_from_HDF5(self, hf, path=lambda f: f'obs/{f}'):
-
-        N = len(hf[f'{path(self.filters[-1])}/flux'][()])
+        N = len(hf[f'{flux_path(self.filters[-1])}'][()])
 
         table = {'#id': np.arange(N)}
 
         for i, f in enumerate(self.filters):
-            table['F'+str(i+1)] = hf[f'{path(f)}/flux'][()]
-            table['E'+str(i+1)] = hf[f'{path(f)}/flux_err'][()]
+            table['F'+str(i+1)] = hf[f'{flux_path(f)}'][()]
+            table['E'+str(i+1)] = hf[f'{flux_err_path(f)}'][()]
 
         def flatten(l): return [item for sublist in l for item in sublist]
         names = ['#id'] + flatten([['F'+str(i+1), 'E'+str(i+1)] for i in range(len(self.filters))])
 
-        ascii.write(table, f'{self.EAZY_working_dir}/inputs/{self.ID}.cat',
+        ascii.write(table, f'{self.eazy_working_dir}/inputs/{self.id}.cat',
                     names=names, overwrite=True)
+
+
+    def create_input_catalogue_from_dict(self, flux_dict, err_dict):
+
+        """
+        Create an EAZY input file from a pair of dictionaries describing the flux and error  
+        """
+
+        filters = list(flux_dict.keys())
+
+        N = len(flux_dict[filters[0]])
+
+        table = {'#id': np.arange(N)}
+
+        for i, f in enumerate(filters):
+            table['F'+str(i+1)] = flux_dict[f]
+            table['E'+str(i+1)] = err_dict[f]
+
+        def flatten(l): return [item for sublist in l for item in sublist]
+        names = ['#id'] + flatten([['F'+str(i+1), 'E'+str(i+1)] for i in range(len(self.filters))])
+
+        ascii.write(table, f'{self.eazy_working_dir}/inputs/{self.id}.cat',
+                    names=names, overwrite=True)
+
 
 
 def create_EAZY_filter_res(fc, filter_res_file='FILTER.RES'):
 
     o = []
 
-    for f in fc.filters:
+    for f in fc:
+        o.append('{n} {f}\n'.format(n=len(f.original_lam), f=f.filter_code))  # filter header
 
-        o.append('{n} {f}\n'.format(n=len(fc.filter[f].lam), f=f))  # filter header
-
-        for i, l in enumerate(fc.filter[f].lam):
-            o.append('{i:>5}   {l}   {t}\n'.format(i=i+1, l=l, t=fc.filter[f].t[i]))
+        for i, l in enumerate(f.original_lam):
+            o.append('{i:>5}   {l}   {t}\n'.format(i=i+1, l=l, t=f.original_t[i]))
 
     open(filter_res_file, 'w').writelines(o)
 
@@ -265,9 +308,9 @@ class template:
     pass
 
 
-def get_templates(template_set, path_to_EAZY=os.getenv('EAZY')):
+def get_templates(template_set, path_to_eaxy=os.getenv('EAZY')):
 
-    params = ascii.read(f'{path_to_EAZY}/templates/{template_set}', names=[
+    params = ascii.read(f'{path_to_eaxy}/templates/{template_set}', names=[
                         'Template number', 'Template file name', 'Lambda_conv', 'Age', 'Template error amplitude'])
     print(params)
 
@@ -275,7 +318,7 @@ def get_templates(template_set, path_to_EAZY=os.getenv('EAZY')):
 
     for template_number, template_file_name, template_age in zip(params['Template number'].data, params['Template file name'].data, params['Age'].data):
 
-        lam, flam = np.loadtxt(f'{path_to_EAZY}/{template_file_name}', unpack=True)
+        lam, flam = np.loadtxt(f'{path_to_eaxy}/{template_file_name}', unpack=True)
 
         t = template()
         t.number = template_number
@@ -299,110 +342,6 @@ def get_template_grid(templates):
         fnu.append(t.fnu)
 
     return lam, np.array(fnu)
-
-
-def default_params(EAZY_working_dir):
-
-    # EAZY Default parameters
-
-    params = {}
-
-    # Filters
-    params['FILTERS_RES'] = 'FILTER.RES.latest'  # Filter transmission data
-    # Format of FILTERS_RES file -- 0: energy-  1: photon-counting detector
-    params['FILTER_FORMAT'] = 1
-    params['SMOOTH_FILTERS'] = 'n'                  # Smooth filter curves with Gaussian
-    params['SMOOTH_SIGMA'] = 100.               # Gaussian sigma (in Angstroms) to smooth filters
-
-    # Templates
-    params['TEMPLATES_FILE'] = f'templates/eazy_v1.2_dusty.spectra.param'  # Template definition file
-    params['TEMPLATE_COMBOS'] = 'a'                  # Template combination options:
-    #         1 : one template at a time
-    #         2 : two templates, read allowed combinations from TEMPLATES_FILE
-    #        -2 : two templates, all permutations
-    # a <or> 99 : all templates simultaneously
-    # Tolerance for non-negative combinations (TEMPLATE_COMBOS=a)
-    params['NMF_TOLERANCE'] = 1.e-4
-    # Wavelength grid definition file
-    params['WAVELENGTH_FILE'] = f'templates/EAZY_v1.1_lines/lambda_v1.1.def'
-    # Template error definition file
-    params['TEMP_ERR_FILE'] = f'templates/TEMPLATE_ERROR.eazy_v1.0'
-    params['TEMP_ERR_A2'] = 0.50               # Template error amplitude
-    params['SYS_ERR'] = 0.00               # Systematic flux error (% of flux)
-    params['APPLY_IGM'] = 'y'                  # Apply Madau 1995 IGM absorption
-    # File containing the Lyman alpha forest data from Inoue
-    params['LAF_FILE'] = f'templates/LAFcoeff.txt'
-    # File containing the damped Lyman absorber data from Inoue
-    params['DLA_FILE'] = f'templates/DLAcoeff.txt'
-    # Scaling of 2175A bump.  Values 0.13 (0.27) absorb ~10 (20) % at peak.
-    params['SCALE_2175_BUMP'] = 0.00
-
-    params['DUMP_TEMPLATE_CACHE'] = 'n'                  # Write binary template cache
-    params['USE_TEMPLATE_CACHE'] = 'n'                  # Load in template cache
-    params['CACHE_FILE'] = 'photz.tempfilt'     # Template cache file (in OUTPUT_DIRECTORY)
-
-    # Input Files
-    params['CATALOG_FILE'] = 'hdfn_fs99_eazy.cat'  # Catalog data file
-    # Catalog photometry in magnitudes rather than f_nu fluxes
-    params['MAGNITUDES'] = 'n'
-    params['NOT_OBS_THRESHOLD'] = -90                # Ignore flux point if <NOT_OBS_THRESH
-    params['N_MIN_COLORS'] = 3                  # Require N_MIN_COLORS to fit
-
-    # Output Files
-    # Directory to put output files in
-    params['OUTPUT_DIRECTORY'] = f'{EAZY_working_dir}/outputs'
-    params['MAIN_OUTPUT_FILE'] = 'photz'              # Main output file, .zout
-    params['PRINT_ERRORS'] = 'y'                  # Print 68, 95 and 99% confidence intervals
-    # Scale ML Chi-squared values to improve confidence intervals
-    params['CHI2_SCALE'] = 1.0
-    # Dump information from the run into [MAIN_OUTPUT_FILE].param
-    params['VERBOSE_LOG'] = 'y'
-    params['OBS_SED_FILE'] = 'n'                  # Write out observed SED/object, .obs_sed
-    params['TEMP_SED_FILE'] = 'n'                   # Write out best template fit/object, .temp_sed
-    params['POFZ_FILE'] = 'n'                   # Write out Pofz/object, .pz
-    # Save OBS_SED, TEMP_SED, PZ in binary format to read with e.g IDL
-    params['BINARY_OUTPUT'] = 'n'
-
-    # Redshift / Mag prior
-    params['APPLY_PRIOR'] = 'n'                  # Apply apparent magnitude prior
-    params['PRIOR_FILE'] = f'templates/prior_K_extend.dat'  # File containing prior grid
-    # Filter from FILTER_RES corresponding to the columns in PRIOR_FILE
-    params['PRIOR_FILTER'] = 28
-    # AB zeropoint of fluxes in catalog.  Needed for calculating apparent mags!
-    params['PRIOR_ABZP'] = 25.0
-
-    # Redshift Grid
-    params['FIX_ZSPEC'] = 'n'                  # Fix redshift to catalog zspec
-    params['Z_MIN'] = 0.01               # Minimum redshift
-    params['Z_MAX'] = 20.00               # Maximum redshift
-    params['Z_STEP'] = 0.01               # Redshift step size
-    params['Z_STEP_TYPE'] = 0  # 0 = ZSTEP, 1 = Z_STEP*(1+z)
-
-    # Zeropoint Offsets
-    # Look for zphot.zeropoint file and compute zeropoint offsets
-    params['GET_ZP_OFFSETS'] = 'n'
-    # Tolerance for iterative fit for zeropoint offsets [not implemented]
-    params['ZP_OFFSET_TOL'] = 1.E-4
-
-    # Rest-frame colors
-    # Comma-separated list of rest frame filters to compute
-    params['REST_FILTERS'] = '---'
-    # Padding (Ang) for choosing observed filters around specified rest-frame pair.
-    params['RF_PADDING'] = 1000.
-    params['RF_ERRORS'] = 'n'                  # Compute RF color errors from p(z)
-    # Redshift to use for rest-frame color calculation (z_a, z_p, z_m1, z_m2, z_peak)
-    params['Z_COLUMN'] = 'z_peak'
-    # Use z_spec when available for rest-frame colors
-    params['USE_ZSPEC_FOR_REST'] = 'y'
-    # Get redshifts from OUTPUT_DIRECTORY/MAIN_OUTPUT_FILE.zbin rather than fitting them.
-    params['READ_ZBIN'] = 'n'
-
-    # Cosmology
-    params['H0'] = 70.0               # Hubble constant (km/s/Mpc)
-    params['OMEGA_M'] = 0.3                # Omega_matter
-    params['OMEGA_L'] = 0.7                # Omega_lambda
-
-    return params
 
 
 def write_param_file(filename, params):
